fa004eee6780efe6c7866a7fb6e8c176bdd9afbe
Shogo_N <underworld.ho.ho@gmail.com>
2014-11-29 22:25:44 +0900

    敵機と自機の衝突判定を正常に直した

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -42,7 +42,7 @@ public class Enemy : MonoBehaviour
 		string layerName = LayerMask.LayerToName(c.gameObject.layer);
 		
 		// レイヤー名がBullet (Player)以外の時は何も行わない
-		if( layerName != "Bullet (Player)") return;
+		if( layerName != "Bullet(Player)") return;
 		
 		// 弾の削除
 		Destroy(c.gameObject);
