34c832fb4bd5ba9e887895908eaf903ce0860635
Shogo_N <underworld.ho.ho@gmail.com>
2014-11-30 15:20:30 +0900

    第１２回終了（実装完了）

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,10 +3,10 @@
 public class Bullet : MonoBehaviour
 {
 	// 弾の移動スピード
-	public int speed = 10;
+	public int speed = 1;
 	
 	// ゲームオブジェクト生成から削除するまでの時間
-	public float lifeTime = 1;
+	public float lifeTime = 5;
 	
 	// 攻撃力
 	public int power = 1;
