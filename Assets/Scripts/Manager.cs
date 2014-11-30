6b07a63b6e4aa7f67f94c8e9b7572fd7f9844343
Shogo_N <underworld.ho.ho@gmail.com>
2014-11-30 19:07:51 +0900

    モバイル編　第３回終了

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -14,10 +14,10 @@ public class Manager : MonoBehaviour
 		title = GameObject.Find ("Title");
 	}
 	
-	void Update ()
+	void OnGUI ()
 	{
-		// ゲーム中ではなく、Xキーが押されたらtrueを返す。
-		if (IsPlaying () == false && Input.GetKeyDown (KeyCode.X)) {
+		// ゲーム中ではなく、タッチまたはマウスクリック直後であればtrueを返す。
+		if (IsPlaying () == false && Event.current.type == EventType.MouseDown) {
 			GameStart ();
 		}
 	}
@@ -31,9 +31,7 @@ public class Manager : MonoBehaviour
 	
 	public void GameOver ()
 	{
-		// ハイスコアの保存
-		FindObjectOfType<Score>().Save();
-		
+		FindObjectOfType<Score> ().Save ();
 		// ゲームオーバー時に、タイトルを表示する
 		title.SetActive (true);
 	}
